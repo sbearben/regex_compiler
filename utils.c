88ef5d70caee479720e4bea3d813a05be4e7a90a
Armon Khosravi <armonk@gmail.com>
2023-12-29 16:02:54 -0500

    Add num_places util method

--- utils.c
+++ utils.c
@@ -1,5 +1,6 @@
 #include "utils.h"
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,6 +11,25 @@ void* xmalloc(size_t size) {
       exit(EXIT_FAILURE);
    }
    return ptr;
+}
+
+// void* array_concat(void* array1, size_t size1, void* array2, size_t size2, size_t element_size) {
+//    void* array = xmalloc((size1 + size2) * element_size);
+//    memcpy(array, array1, size1 * element_size);
+//    memcpy(array + size1 * element_size, array2, size2 * element_size);
+
+//    return array;
+// }
+
+int num_places(int n) {
+   int r = 1;
+   if (n < 0) n = -n;
+
+   while (n > 9) {
+      n /= 10;
+      r++;
+   }
+   return r;
 }
 
 void error(void) {
