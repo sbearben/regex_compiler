ce1003b0bc678c5c5727015e681b522d9052cac5
Armon Khosravi <armonk@gmail.com>
2024-01-21 20:34:42 -0500

    Add initial implementation of ranges (#14)
    * Start impl
    
    * add all ascii characters to config
    
    * Incorporate range

--- parse.c
+++ parse.c
@@ -5,7 +5,8 @@
  * <regexp> -> <concat> { "|" <concat> }
  * <concat> -> <quantifier> { <quantifier> }
  * <quantifier> -> <factor> [ <quantifier-symbol> ]
- * <factor> ( <regexp> ) | Letter
+ * <factor> ( <regexp> ) | Letter | \[<range>\]
+ * <range> -> Letter [ - Letter ] <range>
  * <quantifier-symbol> -> * | + | ?
  * 
  * Inputs a line of text from stdin
@@ -19,13 +20,16 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "utils.h"
 
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
 #define LITERAL_START 32
 #define LITERAL_END 126
+#define ASCII_SIZE 128
 
-const int NUM_LITERALS = LITERAL_END - LITERAL_START;
+const int NUM_LITERALS = LITERAL_END - LITERAL_START + 1;
 
 /**
  * Improvement ideas:
@@ -42,8 +46,9 @@ typedef struct state {
 typedef enum CharacterClass { ANY } CharClass_t;
 
 typedef enum ChracterConfigColumn {
-   SPECIAL_CHARACTER = 0,
-   QUANTIFIER_SYMBOL = 1,
+   VALID_CHARACTER = 0,
+   SPECIAL_CHARACTER = 1,
+   QUANTIFIER_SYMBOL = 2,
 } CCCol_t;
 
 static char peek(state_t*);
@@ -54,6 +59,7 @@ static nfa_t* regexp(state_t*);
 static nfa_t* concat(state_t*);
 static nfa_t* quantifier(state_t*);
 static nfa_t* factor(state_t*);
+static nfa_t* range(state_t*);
 
 // Constructors per parser grammar
 static nfa_t* new_choice_nfa(nfa_t*, nfa_t*);      // 'a|b'
@@ -65,9 +71,11 @@ static nfa_t* new_literal_nfa(char);               // 'a'
 
 // Chracter classes
 static nfa_t* new_character_class(CharClass_t);
+static nfa_t* new_nfa_from_character_set(char*);
 
 // Character helpers
 static int get_character_config(char, CCCol_t);
+static int is_valid_character(char);
 static int is_special_character(char);
 static int is_quantifier_symbol(char);
 static char get_escaped_character(char);
@@ -80,102 +88,135 @@ static bool in_factor_first_set(char);
  *    quantifier_symbol: bool
  * )
 */
-const int CHARACTER_CONFIG[][2] = {
-    /* ' ' */ {0, 0},
-    /* '!' */ {0, 0},
-    /* '"' */ {1, 0},
-    /* '#' */ {0, 0},
-    /* '$' */ {0, 0},
-    /* '%' */ {0, 0},
-    /* '&' */ {0, 0},
-    /* ''' */ {0, 0},
-    /* '(' */ {1, 0},
-    /* ')' */ {1, 0},
-    /* '*' */ {1, 1},
-    /* '+' */ {1, 1},
-    /* ',' */ {0, 0},
-    /* '-' */ {0, 0},
-    /* '.' */ {1, 0},
-    /* '/' */ {0, 0},
-    /* '0' */ {0, 0},
-    /* '1' */ {0, 0},
-    /* '2' */ {0, 0},
-    /* '3' */ {0, 0},
-    /* '4' */ {0, 0},
-    /* '5' */ {0, 0},
-    /* '6' */ {0, 0},
-    /* '7' */ {0, 0},
-    /* '8' */ {0, 0},
-    /* '9' */ {0, 0},
-    /* ':' */ {0, 0},
-    /* ';' */ {0, 0},
-    /* '<' */ {0, 0},
-    /* '=' */ {0, 0},
-    /* '>' */ {0, 0},
-    /* '?' */ {1, 1},
-    /* '@' */ {0, 0},
-    /* 'A' */ {0, 0},
-    /* 'B' */ {0, 0},
-    /* 'C' */ {0, 0},
-    /* 'D' */ {0, 0},
-    /* 'E' */ {0, 0},
-    /* 'F' */ {0, 0},
-    /* 'G' */ {0, 0},
-    /* 'H' */ {0, 0},
-    /* 'I' */ {0, 0},
-    /* 'J' */ {0, 0},
-    /* 'K' */ {0, 0},
-    /* 'L' */ {0, 0},
-    /* 'M' */ {0, 0},
-    /* 'N' */ {0, 0},
-    /* 'O' */ {0, 0},
-    /* 'P' */ {0, 0},
-    /* 'Q' */ {0, 0},
-    /* 'R' */ {0, 0},
-    /* 'S' */ {0, 0},
-    /* 'T' */ {0, 0},
-    /* 'U' */ {0, 0},
-    /* 'V' */ {0, 0},
-    /* 'W' */ {0, 0},
-    /* 'X' */ {0, 0},
-    /* 'Y' */ {0, 0},
-    /* 'Z' */ {0, 0},
-    /* '[' */ {0, 0},
-    /* '\' */ {1, 0},
-    /* ']' */ {0, 0},
-    /* '^' */ {0, 0},
-    /* '_' */ {0, 0},
-    /* '`' */ {0, 0},
-    /* 'a' */ {0, 0},
-    /* 'b' */ {0, 0},
-    /* 'c' */ {0, 0},
-    /* 'd' */ {0, 0},
-    /* 'e' */ {0, 0},
-    /* 'f' */ {0, 0},
-    /* 'g' */ {0, 0},
-    /* 'h' */ {0, 0},
-    /* 'i' */ {0, 0},
-    /* 'j' */ {0, 0},
-    /* 'k' */ {0, 0},
-    /* 'l' */ {0, 0},
-    /* 'm' */ {0, 0},
-    /* 'n' */ {0, 0},
-    /* 'o' */ {0, 0},
-    /* 'p' */ {0, 0},
-    /* 'q' */ {0, 0},
-    /* 'r' */ {0, 0},
-    /* 's' */ {0, 0},
-    /* 't' */ {0, 0},
-    /* 'u' */ {0, 0},
-    /* 'v' */ {0, 0},
-    /* 'w' */ {0, 0},
-    /* 'x' */ {0, 0},
-    /* 'y' */ {0, 0},
-    /* 'z' */ {0, 0},
-    /* '{' */ {0, 0},
-    /* '|' */ {1, 0},
-    /* '}' */ {0, 0},
-    /* '~' */ {0, 0},
+const int CHARACTER_CONFIG[][3] = {
+    // Null character can cause problems, return a value that will fail a `== true` or `== false` check.
+    {-1, -1, -1}, /* '\0' */
+    {0, 0, 0},    /* 'SOH' */
+    {0, 0, 0},    /* 'STX' */
+    {0, 0, 0},    /* 'ETX' */
+    {0, 0, 0},    /* 'EOT' */
+    {0, 0, 0},    /* 'ENQ' */
+    {0, 0, 0},    /* 'ACK' */
+    {0, 0, 0},    /* 'BEL' */
+    {0, 0, 0},    /* 'BS' */
+    {1, 0, 0},    /* '\t' */
+    {1, 0, 0},    /* '\n' */
+    {0, 0, 0},    /* 'VT' */
+    {0, 0, 0},    /* 'FF' */
+    {1, 0, 0},    /* '\r' */
+    {0, 0, 0},    /* 'SO' */
+    {0, 0, 0},    /* 'SI' */
+    {0, 0, 0},    /* 'DLE' */
+    {0, 0, 0},    /* 'DC1' */
+    {0, 0, 0},    /* 'DC2' */
+    {0, 0, 0},    /* 'DC3' */
+    {0, 0, 0},    /* 'DC4' */
+    {0, 0, 0},    /* 'NAK' */
+    {0, 0, 0},    /* 'SYN' */
+    {0, 0, 0},    /* 'ETB' */
+    {0, 0, 0},    /* 'CAN' */
+    {0, 0, 0},    /* 'EM' */
+    {0, 0, 0},    /* 'SUB' */
+    {0, 0, 0},    /* 'ESC' */
+    {0, 0, 0},    /* 'FS' */
+    {0, 0, 0},    /* 'GS' */
+    {0, 0, 0},    /* 'RS' */
+    {0, 0, 0},    /* 'US' */
+    {1, 0, 0},    /* ' ' */
+    {1, 0, 0},    /* '!' */
+    {1, 1, 0},    /* '"' */
+    {1, 0, 0},    /* '#' */
+    {1, 0, 0},    /* '$' */
+    {1, 0, 0},    /* '%' */
+    {1, 0, 0},    /* '&' */
+    {1, 0, 0},    /* ''' */
+    {1, 1, 0},    /* '(' */
+    {1, 1, 0},    /* ')' */
+    {1, 1, 1},    /* '*' */
+    {1, 1, 1},    /* '+' */
+    {1, 0, 0},    /* ',' */
+    {1, 0, 0},    /* '-' */
+    {1, 1, 0},    /* '.' */
+    {1, 0, 0},    /* '/' */
+    {1, 0, 0},    /* '0' */
+    {1, 0, 0},    /* '1' */
+    {1, 0, 0},    /* '2' */
+    {1, 0, 0},    /* '3' */
+    {1, 0, 0},    /* '4' */
+    {1, 0, 0},    /* '5' */
+    {1, 0, 0},    /* '6' */
+    {1, 0, 0},    /* '7' */
+    {1, 0, 0},    /* '8' */
+    {1, 0, 0},    /* '9' */
+    {1, 0, 0},    /* ':' */
+    {1, 0, 0},    /* ';' */
+    {1, 0, 0},    /* '<' */
+    {1, 0, 0},    /* '=' */
+    {1, 0, 0},    /* '>' */
+    {1, 1, 1},    /* '?' */
+    {1, 0, 0},    /* '@' */
+    {1, 0, 0},    /* 'A' */
+    {1, 0, 0},    /* 'B' */
+    {1, 0, 0},    /* 'C' */
+    {1, 0, 0},    /* 'D' */
+    {1, 0, 0},    /* 'E' */
+    {1, 0, 0},    /* 'F' */
+    {1, 0, 0},    /* 'G' */
+    {1, 0, 0},    /* 'H' */
+    {1, 0, 0},    /* 'I' */
+    {1, 0, 0},    /* 'J' */
+    {1, 0, 0},    /* 'K' */
+    {1, 0, 0},    /* 'L' */
+    {1, 0, 0},    /* 'M' */
+    {1, 0, 0},    /* 'N' */
+    {1, 0, 0},    /* 'O' */
+    {1, 0, 0},    /* 'P' */
+    {1, 0, 0},    /* 'Q' */
+    {1, 0, 0},    /* 'R' */
+    {1, 0, 0},    /* 'S' */
+    {1, 0, 0},    /* 'T' */
+    {1, 0, 0},    /* 'U' */
+    {1, 0, 0},    /* 'V' */
+    {1, 0, 0},    /* 'W' */
+    {1, 0, 0},    /* 'X' */
+    {1, 0, 0},    /* 'Y' */
+    {1, 0, 0},    /* 'Z' */
+    {1, 1, 0},    /* '[' */
+    {1, 1, 0},    /* '\' */
+    {1, 1, 0},    /* ']' */
+    {1, 0, 0},    /* '^' */
+    {1, 0, 0},    /* '_' */
+    {1, 0, 0},    /* '`' */
+    {1, 0, 0},    /* 'a' */
+    {1, 0, 0},    /* 'b' */
+    {1, 0, 0},    /* 'c' */
+    {1, 0, 0},    /* 'd' */
+    {1, 0, 0},    /* 'e' */
+    {1, 0, 0},    /* 'f' */
+    {1, 0, 0},    /* 'g' */
+    {1, 0, 0},    /* 'h' */
+    {1, 0, 0},    /* 'i' */
+    {1, 0, 0},    /* 'j' */
+    {1, 0, 0},    /* 'k' */
+    {1, 0, 0},    /* 'l' */
+    {1, 0, 0},    /* 'm' */
+    {1, 0, 0},    /* 'n' */
+    {1, 0, 0},    /* 'o' */
+    {1, 0, 0},    /* 'p' */
+    {1, 0, 0},    /* 'q' */
+    {1, 0, 0},    /* 'r' */
+    {1, 0, 0},    /* 's' */
+    {1, 0, 0},    /* 't' */
+    {1, 0, 0},    /* 'u' */
+    {1, 0, 0},    /* 'v' */
+    {1, 0, 0},    /* 'w' */
+    {1, 0, 0},    /* 'x' */
+    {1, 0, 0},    /* 'y' */
+    {1, 0, 0},    /* 'z' */
+    {1, 0, 0},    /* '{' */
+    {1, 1, 0},    /* '|' */
+    {1, 0, 0},    /* '}' */
+    {1, 0, 0},    /* '~' */
 };
 
 /**
@@ -267,9 +308,58 @@ static nfa_t* factor(state_t* state) {
    } else if (peek(state) == '.') {
       match(state, '.');
       temp = new_character_class(ANY);
+   } else if (peek(state) == '[') {
+      match(state, '[');
+      temp = range(state);
+      match(state, ']');
    } else {
       error("[factor] Unexpected token");
    }
+   return temp;
+}
+
+static nfa_t* range(state_t* state) {
+   static char seen_characters[ASCII_SIZE];
+   memset(seen_characters, 0, sizeof seen_characters);
+   int max_character_count = 0;
+
+   // TODO: - if negation is implemented ('^'), then we need to handle escaping it to match '^' literally
+   //       - seems it only needs to be escaped if it is the first character in the range
+   //       - seems anything can be escaped and it's just treated as a literal
+   while (peek(state) != ']') {
+      char start = next(state);
+      if (peek(state) == '-') {
+         match(state, '-');
+         if (is_valid_character(peek(state)) == false) {
+            error("Invalid character in range");
+         }
+         char end = next(state);
+         if (start > end) {
+            continue;
+         }
+         for (char c = start; c <= end; c++) {
+            seen_characters[(int)c] = 1;
+         }
+         max_character_count += end - start + 1;
+      } else {
+         seen_characters[(int)start] = 1;
+         max_character_count++;
+      }
+   }
+
+   // Create nfa from seen characters
+   char* characters = (char*)xmalloc((MIN(max_character_count, ASCII_SIZE) + 1) * sizeof(char));
+   int ch_index = 0;
+   for (int i = 0; i < sizeof seen_characters; i++) {
+      if (seen_characters[i] == 1) {
+         characters[ch_index++] = i;
+      }
+   }
+   characters[ch_index] = '\0';
+
+   nfa_t* temp = new_nfa_from_character_set(characters);
+   free(characters);
+
    return temp;
 }
 
@@ -465,32 +555,49 @@ static nfa_t* new_literal_nfa(char value) {
    return nfa;
 }
 
+/**
+ * Character classes
+*/
+
 static nfa_t* new_character_class(CharClass_t cctype) {
-   // '.' matches any single character except line terminators \n, \r (but includes tabs)
-   // - multiply by 3 just to overallocate space for characters that require escaping,
-   //   as well as for joining pipe characters
-   static char any_cc_regex_string[NUM_LITERALS * 3] = {0};
+   // '.' matches any single character except line terminators \n, \r (but includes \t)
+   static char any_characters[NUM_LITERALS + 2] = {0};
 
    switch (cctype) {
       case ANY:
-         if (any_cc_regex_string[0] == 0) {
+         if (any_characters[0] == 0) {
             int index_offset = 0;
-            for (char cl = 0; cl < NUM_LITERALS; cl++) {
-               if (is_special_character(cl + LITERAL_START) == true) {
-                  any_cc_regex_string[index_offset++] = '\\';
-               }
-               any_cc_regex_string[index_offset++] = cl + LITERAL_START;
-               any_cc_regex_string[index_offset++] = '|';
+            for (char cl = LITERAL_START; cl <= LITERAL_END; cl++) {
+               any_characters[index_offset++] = cl;
             }
-            any_cc_regex_string[index_offset++] = '\\';
-            any_cc_regex_string[index_offset++] = 't';
-            any_cc_regex_string[index_offset] = '\0';
+            any_characters[index_offset++] = '\t';
+            any_characters[index_offset] = '\0';
          }
-         return parse_regex_to_nfa(any_cc_regex_string);
+         return new_nfa_from_character_set(any_characters);
       default:
          error("[new_character_class] unexpected character class");
    }
    return NULL;
+}
+
+static nfa_t* new_nfa_from_character_set(char* characters) {
+   // Over-allocate space for escaping characters and joining pipe characters
+   static char ch_set_regex_str[LITERAL_END * 3] = {0};
+   memset(ch_set_regex_str, 0, sizeof ch_set_regex_str);
+
+   int write_index = 0;
+   for (char* chptr = characters; *chptr != '\0'; chptr++) {
+      if (is_special_character(*chptr) == true) {
+         ch_set_regex_str[write_index++] = '\\';
+      }
+      ch_set_regex_str[write_index++] = *chptr;
+      if (*(chptr + 1) != '\0') {
+         ch_set_regex_str[write_index++] = '|';
+      }
+   }
+   ch_set_regex_str[write_index] = '\0';
+
+   return parse_regex_to_nfa(ch_set_regex_str);
 }
 
 /**
@@ -498,17 +605,20 @@ static nfa_t* new_character_class(CharClass_t cctype) {
 */
 
 static int get_character_config(char c, CCCol_t col) {
-   if (c < LITERAL_START || c > LITERAL_END) {
-      // Null character can cause problems, return a value that will fail a `== true` or `== false` check.
+   if (c < 0 || c > LITERAL_END) {
       return -1;
    }
-   return CHARACTER_CONFIG[(int)c - LITERAL_START][col];
+   return CHARACTER_CONFIG[(int)c][col];
 }
+
+static int is_valid_character(char c) { return get_character_config(c, VALID_CHARACTER); }
 
 static int is_special_character(char c) { return get_character_config(c, SPECIAL_CHARACTER); }
 
 static int is_quantifier_symbol(char c) { return get_character_config(c, QUANTIFIER_SYMBOL); }
 
+// NOTE: I don't know if this is needed, but it's here for now
+// Q: Why would a regex be written as '\\t' to match a tab instead of writing a tab character directly?
 static char get_escaped_character(char c) {
    switch (c) {
       case 't':
@@ -524,5 +634,6 @@ static char get_escaped_character(char c) {
 
 // Whether the character is in the first set of 'factor'
 static bool in_factor_first_set(char c) {
-   return is_special_character(c) == false || c == '(' || c == '\\' || c == '.';
+   return (is_valid_character(c) == true && is_special_character(c) == false) || c == '(' ||
+          c == '\\' || c == '.' || c == '[';
 }
